27b04a01b3cc95012890ae545bc709ffec844576
Кирилл Дегтярев <degtiarev.kd@phystech.edu>
2023-10-10 17:23:37 +0300

    reference.md added, actions ideas explained

--- Simulator/include/action.h
+++ Simulator/include/action.h
@@ -1,9 +1,17 @@
 #pragma once
 #include"diceroll.h"
+#include"character.h"
 #include<string>
 
-struct Action
+class Action
 {
     std::string actionName_;
-    DiceRoll damage_; // starting with constant damage
+    DiceRoll damage_;
+    DiceRoll attack_;
+
+public:
+    Action(std::string actionName_, DiceRoll damage_, DiceRoll attack_);
+    void useOn(CharSheet& targetChar, int attackBonus = 0, int damage_bonus = 0);
 };
+
+Action actionFactory(std::string actionName_);

6e6e2097d58c3ad332034f5dc204c17ee91fc538
Кирилл Дегтярев <degtiarev.kd@phystech.edu>
2023-10-17 21:34:29 +0300

    tests now in main

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -4,8 +4,41 @@
 #include "dice.h"
 #include <iostream>
 
+void diceTest();
+
 int main(int argc, char* argv[])
 {
-    std::cout<<"Hello world!"<<std::endl;
+    diceTest();
     return EXIT_SUCCESS;
+}
+
+void diceTest()
+{
+    std::cout << "ROLLING DICE TEST" << std::endl;
+
+    std::cout << "Simple 1d20 dice testing ::" << std::endl;
+    for(int i = 0; i < 10; i++)
+        std::cout << "result of dice.roll is " << Dice().roll() << std::endl;
+
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "2d6 dice testing ::" << std::endl;
+    for(int i = 0; i < 10; i++)
+        std::cout << "result of dice.roll is " << Dice(2,6).roll() << std::endl;
+
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "8d12 dice testing ::" << std::endl;
+    for(int i = 0; i < 10; i++)
+        std::cout << "result of dice.roll is " << Dice(8,12).roll() << std::endl;
+
+
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "1d2+10 dice testing ::" << std::endl;
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout<<"result of dice.roll is " << Dice(1,2,10).roll() <<std::endl;
+    }
+
+    std::cout << "ROLLING DICE TEST END" << std::endl;
 }
